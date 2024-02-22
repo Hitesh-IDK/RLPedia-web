8b7a896bd2db69fc91a478d8bdf41407b5fc9561
Shreeram N S <nsshreeram@gmail.com>
2024-02-22 21:48:11 +0530

    homepage ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
+import Login from "./components/login"
 import "./App.css";
+import Banner from "./components/Banner";
 import GetStarted from "./components/GetStarted/get-started";
 
 function App() {
   return (
     <div>
-      <GetStarted />
+      <Login/>
     </div>
   );
 }
