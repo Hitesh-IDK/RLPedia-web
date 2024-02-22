8ccd19a10127687c47cd59bec74659ae7d67a553
Hitesh Parmar <10hiteshparmar@gmail.com>
2024-02-22 14:15:15 +0530

    color change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import GetStarted from "./components/GetStarted/get-started";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <GetStarted />
+    </div>
+  );
 }
 
 export default App;
